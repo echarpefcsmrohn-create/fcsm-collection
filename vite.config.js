a71cc943b10043e00ece50470ff9383145cbe90b
echarpefcsmrohn-create <echarpe.fcsm.rohn@gmail.com>
2026-06-14 19:35:31 +0200

    v1.119

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { execSync } from 'child_process'
-
-let commitCount = '48'
-let commitHash = ''
-try {
-  commitCount = execSync('git rev-list --count HEAD').toString().trim()
-  commitHash = execSync('git rev-parse --short HEAD').toString().trim()
-} catch(e) {}
 
 export default defineConfig({
   plugins: [react()],
   define: {
-    __APP_VERSION__: JSON.stringify(`1.${commitCount}`),
-    __COMMIT_HASH__: JSON.stringify(commitHash),
+    __APP_VERSION__: JSON.stringify('1.119'),
   }
 })
